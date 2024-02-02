c3bf48fedc950e20d3ab919f5f3ee72244e861a8
Konstantin1spb <kevlen02@mail.ru>
2024-02-02 22:27:28 +0300

    second commit without JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,29 +1,39 @@
-// декларативный начало
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 let currentYear = new Date().getFullYear();
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div>{currentYear}</div>
-			</header>
-		</div>
+
+const App = () => {
+	return createElement(
+		'div',
+		{
+			className: 'App',
+		},
+		createElement(
+			'header',
+			{
+				className: 'App-header',
+			},
+			createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			createElement('p', null, 'Edit src/App.js and save to reload.'),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('div', null, currentYear),
+		),
 	);
-}
+};
 
 export default App;
-// декларативный конец
